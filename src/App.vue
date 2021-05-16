2c268049ce53bbe351452403436568b0cfbcda74
eftorregoza <exxeltorregoza@gmail.com>
2021-05-16 11:22:59 +0800

    modified:   package-lock.json 	modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About 3</router-link>
+      <router-link to="/about">About L3-start</router-link>
     </div>
     <router-view />
   </div>
